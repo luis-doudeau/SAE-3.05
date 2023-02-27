83371b0de7b4f5649b5824d60e265bccc52ece49
CHARPENTIER Maxym <o22103433@info25-09.iut45.univ-orleans.fr>
2023-02-27 12:43:54 +0100

    Ajout info mail

--- Developpement/app/app.py
+++ Developpement/app/app.py
@@ -9,11 +9,14 @@ app.config['SECRET_KEY'] = token_urlsafe(16)
 login_manager = LoginManager(app)
 login_manager.login_view = 'connexion'
 
-mail = Mail(app)
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+
+app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'bdboum45@gmail.com'
-app.config['MAIL_PASSWORD'] = 'llmbdboum'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+#app.config['MAIL_PASSWORD'] = 'llmbdboum'
+app.config['MAIL_PASSWORD'] = 'otpvlohopdtlvrjz'
+#app.config['TESTING'] = False
+#app.config['MAIL_USE_TLS'] = False
+#app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
